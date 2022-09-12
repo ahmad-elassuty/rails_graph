22c4080a5ca56caf74cbd0f3ad056f003bc49f6d
Ahmad Elassuty <ahmad.elassuty@gmail.com>
2022-09-12 17:51:54 +0200

    init RailsGraph

---
+++ lib/rails_graph.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "rails_graph/version"
+
+module RailsGraph
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/rails_graph_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe RailsGraph do
+  it "has a version number" do
+    expect(RailsGraph::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
